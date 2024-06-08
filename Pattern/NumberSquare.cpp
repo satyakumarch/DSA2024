92843d9ba5a4ba299aca2b5975513174f8f064db
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-08 18:40:42 +0545

    square number

--- Pattern/NumberSquare.cpp
+++ Pattern/NumberSquare.cpp
@@ -4,11 +4,9 @@ int main(){
     int n;
     cout<<"Enter the number:";
     cin>>n;
-    for(int row=1;row<=n;row++)
-    {
-        for(int col=1;col<=n;col++)
-        {
-            cout<<"*"<<" ";
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            cout<<row<<" ";
         }
         cout<<endl;
     }

1ccdf72c7b58cced3af1a75e6db677c715602b2e
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-08 11:10:29 +0545

    multiplication table

---
+++ ForLoop/multiplicationTable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        cout<<n<<" x "<<i<<" = "<<i*n<<endl;
+    }
+    return 0;
+}

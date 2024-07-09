9bb7d2b9da4804e2a990aeab669ee0dc4dbd61c3
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-09 21:58:56 +0545

    undirected weighted graph

---
+++ RaghavPWSKILLDSA/Graph/undirected unweighted graph.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector> // Include the vector header
+using namespace std;
+
+// Adjacency matrix
+// Undirected unweighted graph
+
+int main() {
+    int vertex, edges;
+    cin >> vertex >> edges;
+
+    // Correct the syntax for declaring a 2D vector
+    vector<vector<bool>> AdjMat(vertex, vector<bool>(vertex, 0));
+
+    int u, v;
+    for(int i = 0; i < edges; i++) {
+        cin >> u >> v;
+        AdjMat[u][v] = 1;
+        AdjMat[v][u] = 1;
+    }
+
+    // Correct the typo in the inner loop from 'vertix' to 'vertex'
+    for(int i = 0; i < vertex; i++) {
+        for(int j = 0; j < vertex; j++)
+            cout << AdjMat[i][j] << " ";
+        cout << endl;
+    }
+}
+//undirected unweightted graph
+5 6
+0 1
+0 2
+1 2
+1 3
+2 4
+3 4
+0 1 1 0 0
+1 0 1 1 0
+1 1 0 0 1
+0 1 0 0 1
+0 0 1 1 0

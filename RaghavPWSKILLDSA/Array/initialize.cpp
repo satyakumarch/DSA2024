2cbc99518edc0522721f4bb017d088af3ca3ae4d
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-18 14:45:40 +0545

    array initialization

---
+++ RaghavPWSKILLDSA/Array/initialize.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+
+int main()
+{
+    // int arr[5]={1,2,3};
+    // for(int i=0;i<5;i++)
+    //     cout<<arr[i]<<" ";
+
+
+
+    int arr[6];
+    cout<<"Enter the element in array:";
+    for(int i=0;i<6;i++)
+    cin>>arr[i];
+
+    for(int i=0;i<6;i++)
+    cout<<arr[i]<<" ";
+        return 0;
+    
+}

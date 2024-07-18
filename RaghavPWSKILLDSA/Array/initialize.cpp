ab67178efdc2f1c72aac981e05ea321d54ad9793
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-18 15:15:15 +0545

    max and min element

--- RaghavPWSKILLDSA/Array/initialize.cpp
+++ RaghavPWSKILLDSA/Array/initialize.cpp
@@ -11,15 +11,35 @@ int main()
 
 
 
-    int arr[6];
-    cout<<"Enter the element in array:";
-    for(int i=0;i<6;i++)
-    cin>>arr[i];
+    // int arr[6];
+    // cout<<"Enter the element in array:";
+    // for(int i=0;i<6;i++)
+    // cin>>arr[i];
 
-    for(int i=0;i<6;i++)
-    cout<<arr[i]<<" ";
-    cout<<endl;
-    cout<<"Size of array:"<<sizeof(arr);
-        return 0;
-    
+    // for(int i=0;i<6;i++)
+    // cout<<arr[i]<<" ";  
+    // cout<<endl;
+    // cout<<"Size of array:"<<sizeof(arr);
+
+
+
+    int arr[5]={2,3,1,7,8};
+    int ans=INT16_MAX;
+    //min value
+    for(int i=0;i<5;i++)
+    {
+        if(arr[i]<ans)
+        ans=arr[i];
+    }
+    cout<<ans<<endl;
+
+    //max value
+    ans=INT16_MIN;
+    for(int i=0;i<5;i++)
+    {
+        if(arr[i]>ans)
+        ans=arr[i];
+    }
+    cout<<ans<<endl;
+    return 0;
 }

6478c0926ba9b97264b97a9ad3c51e3f2c0fdba8
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-10-07 23:56:24 +0530

    radius of the circle

---
+++ DSA2024/RaghavPWDSA/Graph/context.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	    int r,g,b;
+	    cin>>r>>g>>b;
+	    int sum=r+g+b;
+	    if(sum%2!=0){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	
+	return 0;
+}
